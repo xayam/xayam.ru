0fc7dda7a312ebcb1846abd9898e996635b603a7
xayam <xayam@yandex.ru>
2025-05-25 03:59:32 +0300

    v0.5

--- docs/portfolio/xportfolio.py
+++ docs/portfolio/xportfolio.py
@@ -2,94 +2,76 @@
 
 from __init__ import *
 
-import json
 import os.path
 import re
 
 import telebot
+from telebot import types
+
 
 bot = telebot.TeleBot(TELEGRAM_BOT_API_KEY)
 
 
-# bot.send_message(
-#             chat_id=message.chat.id,
-#             text="<b>История очищена.</b> Начните диалог с Абсолярисом заново. Океан ждет...",
-#             parse_mode="HTML",
-#             reply_to_message_id=message.message_id,
-#             allow_sending_without_reply=False,
-#         )
-
-def command_start(message, data):
+def command_start(command="/start", message=None):
     pass
 
-def command_projects(message, data):
+def command_html(command="/html", message=None):
     pass
 
-def command_links(message, data):
+def command_pdf(command="/pdf", message=None):
     pass
 
-def command_skills(message, data):
-    pass
 
-def command_tools(message, data):
-    pass
-
-def command_education(message, data):
-    pass
-
-def command_experience(message, data):
-    pass
-
-def command_interests(message, data):
-    pass
-
-def command_disease(message, data):
-    pass
-
-def command_autobiography(message, data):
-    pass
-
-def command_status(message, data):
-    pass
-
-def command_html(message, data):
-    pass
-
-def command_pdf(message, data):
-    pass
-
-commands = {
-    "/start": { "handler": command_start, "name": "Старт", "view": False, },
-    "/projects": { "handler": command_projects, "name": "Проекты", "view": True, },
-    "/links": { "handler": command_links, "name": "Ссылки", "view": True, },
-    "/skills": { "handler": command_skills, "name": "Навыки", "view": True, },
-    "/tools": { "handler": command_tools, "name": "Инструменты", "view": True, },
-    "/education": { "handler": command_education, "name": "Образование", "view": True, },
-    "/experience": { "handler": command_experience, "name": "Опыт работы", "view": True, },
-    "/interests": { "handler": command_interests, "name": "Интересы", "view": True, },
-    "/disease": { "handler": command_disease, "name": "Болезнь", "view": True, },
-    "/autobiography": { "handler": command_autobiography, "name": "Автобиография", "view": True, },
-    "/status": { "handler": command_status, "name": "Статус", "view": True, },
-    "/html": { "handler": command_html, "name": "HTML", "view": True, },
-    "/pdf": { "handler": command_pdf, "name": "PDF", "view": True, },
-}
-
-@bot.message_handler(content_types=['text'])
-def process_commands(message):
+def menu_execute(command, message):
     try:
-        data_file = "xportfolio/" + message.text[1:] + ".html"
+        data_file = os.getcwd() + "/" + command.text[1:] + ".html"
+        print(data_file)
+        info = "В базе бота отсутствует информация по данной команде."
         if os.path.exists(data_file):
             with open(data_file, mode="r", encoding="UTF-8") as f:
-                data = f.read()
-            info = commands[message.text](message=message, data=data)
+                info = f.read()
     except KeyError:
-        info = "Ошибка. Команда не распознана. Выберите корректную команду в меню."
+        info = "Ошибка. Команда не корректна. Выберите правильную команду в меню."
     bot.send_message(
         chat_id=message.chat.id,
         text=info,
         parse_mode="HTML",
         reply_to_message_id=message.message_id,
         allow_sending_without_reply=False,
+    )
+
+commands = {
+    "/projects": { "handler": menu_execute, "name": "Проекты"},
+    "/links": { "handler": menu_execute, "name": "Ссылки"},
+    "/skills": { "handler": menu_execute, "name": "Навыки"},
+    "/tools": { "handler": menu_execute, "name": "Инструменты"},
+    "/education": { "handler": menu_execute, "name": "Образование"},
+    "/experience": { "handler": menu_execute, "name": "Опыт работы"},
+    "/interests": { "handler": menu_execute, "name": "Интересы"},
+    "/disease": { "handler": menu_execute, "name": "Болезнь"},
+    "/autobiography": { "handler": menu_execute, "name": "Автобиография"},
+    "/status": { "handler": menu_execute, "name": "Статус"},
+    "/html": { "handler": command_html, "name": "HTML"},
+    "/pdf": { "handler": command_pdf, "name": "PDF"},
+}
+
+def menu_create(message) -> types.InlineKeyboardMarkup:
+    markup = types.InlineKeyboardMarkup()
+    btn_list = dict()
+    for command in commands:
+        btn_list[commands[command]["name"]] = \
+            lambda: commands[command]["handler"](command, message)
+    for element in btn_list.items():
+        btn = types.InlineKeyboardButton(text=element[0], callback_data=element[1])
+        markup.add(btn)
+    return markup
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(
+        chat_id=message.chat.id,
+        text="Добро пожаловать.",
+        reply_markup=menu_create(message)
     )
 
 
