4617941d572d37bd51ef825ef611644005a8e642
xayam <xayam@yandex.ru>
2025-10-25 10:06:03 +0300

    v0.188

---
+++ build/build_index.py
@@ -0,0 +1,8 @@
+
+
+# <tr style="border-bottom: 1px solid gray;">
+#             <td>Name</td>
+#             <td>Last modified</td>
+#             <td>Size</td>
+#             <a href="chess-boards/">chess-boards/</a> 2025-10-25 08:14
+#         </tr>
