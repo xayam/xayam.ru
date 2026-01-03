2e8f4f3f07ab9be9064098c2e2db5620f195ebb0
xayam <xayam@yandex.ru>
2026-01-03 14:45:40 +0300

    v1.3.24

--- docs/x/xmap/xlab/xgraver/cutlabx/nc2nc.py
+++ docs/x/xmap/xlab/xgraver/cutlabx/nc2nc.py
@@ -30,44 +30,38 @@ def optimize_gcode(input_path, output_path, optimizer_func, speed):
             x = extract_coord(line, 'X')
             y = extract_coord(line, 'Y')
             points.append((x, y, line))
-        else:
-            points.append((None, None, line))
+        # else:
+        #     points.append((None, None, line))
 
     # Применяем оптимизатор к списку точек
     optimized_points = optimizer_func(points)
 
-    # Перестраиваем body: сопоставляем оптимизированный порядок с исходными строками
-    # (здесь возможны упрощения: например, перегенерация строк по новым координатам)
-    new_body = []
-    for pt in optimized_points:
-        # Генерация строки G1 X... Y... Z... (сохраняя исходный стиль)
-        new_line = ""
-        if pt[0] is not None:
-            new_line += f"G1X{pt[0]:.3f}"
-        if pt[1] is not None:
-            new_line += f"Y{pt[1]:.3f}"
-        new_line += "\n"
-        # print(pt)
-        new_line = pt[2]
-        new_body.append(new_line)
-
     # Сборка итогового файла
-    with open(output_path, 'w') as f:
+    with open(output_path, 'w', encoding="UTF-8") as f:
         f.write(preamble)
         f.write("\n")
-        f.write("\n".join(new_body).replace("\n\n", "\n").strip())
+        f.write(";L0\n")
+        f.write(optimized_points)
         f.write("\n")
         f.write(postamble)
 
 def optimize(points: list):
     width, height, trajectory = get_trajectory("input.png")
-    result = []
-    print(width, height)
-    print(trajectory[0][0])
-    for idx in range(width * height):
+    result = ""
+    print(len(points))
+    for idx in range(len(trajectory)):
+        print(f"{idx}/{len(trajectory)}")
         ys, xs = trajectory[:idx + 1, 0], trajectory[:idx + 1, 1]
-        print(ys, xs)
-    return points
+        y, x = ys[0] / height * 210, xs[0] / width * 297
+        y = round(y, 2)
+        x = round(x, 2)
+        result += f"G0X{x}Y{y}S1000.00F1000.00\n"
+        ys, xs = 210 - ys / height * 210, xs / width * 297
+        ys = ys.round(2)
+        xs = xs.round(2)
+        for i in range(1, len(ys)):
+            result += f"G1X{xs[i]}Y{ys[i]}\n"
+    return result
 
 if __name__ == "__main__":
     optimize_gcode(
