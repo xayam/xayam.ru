1f418dae2d359a967320a8fd0e7f24a4d5a6503c
xayam <xayam@yandex.ru>
2026-01-04 10:40:42 +0300

    v1.3.27

--- docs/x/xmap/xlab/xgraver/cutlabx/nc2nc.py
+++ docs/x/xmap/xlab/xgraver/cutlabx/nc2nc.py
@@ -9,38 +9,21 @@ from png2gif import get_trajectory
 def optimize(filename, speed=1000):
     width, height, binary_image, trajectory = get_trajectory(filename)
     result = ""
-    directions = [
-        [1, 1], [0, 1], [-1, 1], [-1, 0],
-        [-1, -1], [0, -1], [1, -1], [1, 0]
-    ]
+    # directions = [
+    #     [1, 1], [0, 1], [-1, 1], [-1, 0],
+    #     [-1, -1], [0, -1], [1, -1], [1, 0]
+    # ]
     binary_image = 255 - binary_image
     for index in range(len(trajectory)):
         print(f"{index + 1}/{len(trajectory)}")
         cluster = trajectory[index]
         y = round(210.0 - cluster[0][0] / height * 210.0 + 50.1, 2)
         x = round(cluster[0][1] / width * 297.0 + 5.1, 2)
-        ys_pred0, xs_pred0, ys_pred1, xs_pred1= y, x, y, x
+        ys_pred1, xs_pred1 = y, x
         result += f"G0X{x}Y{y}S{speed}.00F1000.00\n"
         for ys, xs in cluster:
-            count = 0
-            for d in directions:
-                if binary_image[ys + d[1]][xs + d[0]] == 0:
-                    count += 1
-            if count == 2:
-                s = "G0"
-                if x != xs_pred0:
-                    s += f"X{x}"
-                    xs_pred0 = x
-                if y != ys_pred0:
-                    s += f"Y{y}"
-                    ys_pred0 = y
-                if s == "G0":
-                    s = ""
-                result += s + "\n"
-
             y = round(210.0 - ys / height * 210.0 + 50.1, 2)
             x = round(xs / width * 297.0 + 5.1, 2)
-
             s = "G1"
             if x != xs_pred1:
                 s += f"X{x}"
@@ -50,7 +33,8 @@ def optimize(filename, speed=1000):
                 ys_pred1 = y
             if s == "G1":
                 s = ""
-            result += s + "\n"
+            s += "\n" + s.replace("G1", "G0") + "\n"
+            result += s
     return result
 
 
@@ -73,4 +57,3 @@ def get_gcode(filename: str, preamble: str, postamble: str, speed: int):
 
 if __name__ == "__main__":
     get_gcode("input.png","begin.nc","end.nc",1000)
-    winsound.Beep(1400, 1000)
