5fa166cdacd0ef4127dedc431153500a1d18f687
xayam <xayam@yandex.ru>
2026-01-26 19:37:01 +0300

    v1.4.16

--- docs/x/xmap/author/playground/build_games.py
+++ docs/x/xmap/author/playground/build_games.py
@@ -5,30 +5,28 @@ def load_config(config_json: str):
     with open(config_json, 'r', encoding='utf-8') as f:
         return json.load(f)
 
-def read_list(template, config, what):
+def read_list(template, config):
     begin_end = {
         'css': ['<style>', '</style>'],
         'js': ['<script type="text/javascript">', '</script>'],
-        'board': ['', ''],
+        'field': ['<script type="text/javascript"> let field = ', '; </script>']
     }
-    result = begin_end[what][0] + '\n'
-    for c in config[what]:
-        try:
-            with open(c, mode='r', encoding='utf-8') as f:
-                result += f.read() + '\n\n'
-        except FileNotFoundError:
-            print(f"ERROR! File {c} not exists!")
-    result += '\n' + begin_end[what][1] + '\n\n'
-    result = template.replace('{{{' + what + '}}}', result, 1)
-    return result
+    for what in begin_end:
+        result = begin_end[what][0] + '\n'
+        for c in config[what]:
+            try:
+                with open(c, mode='r', encoding='utf-8') as f:
+                    result += f.read() + '\n\n'
+            except FileNotFoundError:
+                print(f"ERROR! File {c} not exists!")
+        result += '\n' + begin_end[what][1] + '\n\n'
+        template = template.replace('{{{' + what + '}}}', result, 1)
+    return template
 
 def build(config):
     with open(config['input'], mode='r', encoding='utf-8') as f:
         template = f.read()
-
-    result = read_list(template, config, 'css')
-    result = read_list(result, config, 'js')
-
+    result = read_list(template, config)
     with open(config['output'], mode='w', encoding='utf-8') as f:
         f.write(result)
 
