faf58f6762007f797cbf0ea0df28ffd2d0c469fc
xayam <xayam@yandex.ru>
2026-01-30 06:01:54 +0300

    v1.7.11

--- docs/x/xmap/xlab/xgraver/cutlabx/png2nc.py
+++ docs/x/xmap/xlab/xgraver/cutlabx/png2nc.py
@@ -1,9 +1,66 @@
-import re
-import sys
 import numpy as np
 import cv2
-from Scripts import winsound
+import winsound
+
 from png2gif import get_trajectory
+
+#    Шаблоны заданий работы гравера:
+# 1) WOOD - установлен синий лазер;
+# 2) PLASTIC/METAL - установлен красный лазер;
+# 3) GRAVE - гравировка (один проход);
+# 4) BURN - резка (несколько проходов по кластерам, обычно хватает 5 раз);
+WOOD_GRAVE = "wood_grave"
+WOOD_BURN = "wood_burn"
+PLASTIC_GRAVE = "plastic_grave"
+METAL_GRAVE = "metal_grave"
+METAL_BURN = "metal_burn"
+
+# ввод задания для гравера здесь
+task = METAL_GRAVE
+
+# калибровка по точке привязки
+boundX = 5.1
+boundY = 50.1
+
+# приведение к формату А4
+widthA4 = 297.0
+heightA4 = 210.0
+
+SPEED = "speed" # скорость передвижения лазера
+POWER = "power" # мощность включения лазера в процентах
+LOOP = "loop"   # количество проходов
+
+# Все конфигурации заданий работы гравера
+config = {
+    WOOD_GRAVE: {
+        SPEED: 4000,
+        POWER: 95,
+        LOOP: 1
+    },
+    WOOD_BURN: {
+        SPEED: 400,
+        POWER: 95,
+        LOOP: 5
+    },
+    PLASTIC_GRAVE: {
+        SPEED: 1000,
+        POWER: 95,
+        LOOP: 1
+    },
+    METAL_GRAVE: {
+        SPEED: 1000,
+        POWER: 95,
+        LOOP: 1
+    },
+    METAL_BURN: {
+        SPEED: 400,
+        POWER: 95,
+        LOOP: 5
+    }
+}
+
+# не менять
+task = config[task]
 
 
 def line_pixels(x0, y0, x1, y1):
@@ -26,7 +83,7 @@ def line_pixels(x0, y0, x1, y1):
             y0 += sy
     return points
 
-def optimize(filename, speed=""):
+def optimize(filename: str, speed: str, loop: int = 1) -> str:
     width, height, binary_image, trajectory = \
         get_trajectory(filename=filename, animate=False)
     image = np.ones_like(binary_image) * 255
@@ -35,64 +92,56 @@ def optimize(filename, speed=""):
     # image = cv2.imread("temp.png", cv2.IMREAD_GRAYSCALE)
     # binary_image = 255 - binary_image
     result = ""
-    for index in range(len(trajectory)):
-        print(f"{index + 1}/{len(trajectory)}")
-        cluster = trajectory[index]
-        y = round(210.0 - cluster[0][0] / height * 210.0 + 50.1, 2)
-        x = round(cluster[0][1] / width * 297.0 + 5.1, 2)
-        ys_pred1, xs_pred1 = y, x
-        ys_pred2, xs_pred2 = cluster[0][0], cluster[0][1]
-        result += f"G0X{x}Y{y}{speed}\n"
-        index = -1
-        for ys, xs in cluster:
-            index += 1
-            flag = False
-            y = round(210.0 - ys / height * 210.0 + 50.1, 2)
-            x = round(xs / width * 297.0 + 5.1, 2)
-            line_pts = line_pixels(xs, ys, xs_pred2, ys_pred2)
-            for yy, xx in line_pts:
-                if binary_image[yy, xx] == 0:
-                    flag = True
-                    break
-            if flag:
-                s = "G0"
-            else:
-                s = "G1"
-            if x != xs_pred1:
-                s += f"X{x}"
-                xs_pred1 = x
-            if y != ys_pred1:
-                s += f"Y{y}"
-                ys_pred1 = y
-            if s in ["G0", "G1"]:
-                s = ""
-            if not flag:
-                image = cv2.line(image,
-                                 (xs, ys), (xs_pred2, ys_pred2),
-                                 (0, 0, 0), thickness=1)
-            ys_pred2, xs_pred2 = ys, xs
-            s += "\n" + s.replace("G1", "G0") + "\n"
-            result += s
-    cv2.imwrite("temp.png", image)
+    for i in range(len(trajectory)):
+        for l in range(loop):
+            print(f"{i + 1}/{len(trajectory)} : {l + 1}/{loop}")
+            cluster = trajectory[i]
+            y = round(heightA4 - cluster[0][0] / height * heightA4 + boundY, 2)
+            x = round(cluster[0][1] / width * widthA4 + boundX, 2)
+            ys_pred1, xs_pred1 = y, x
+            ys_pred2, xs_pred2 = cluster[0][0], cluster[0][1]
+            result += f"G0X{x}Y{y}{speed}\n"
+            index = -1
+            for ys, xs in cluster:
+                index += 1
+                flag = False
+                y = round(heightA4 - ys / height * heightA4 + boundY, 2)
+                x = round(xs / width * widthA4 + boundX, 2)
+                line_pts = line_pixels(xs, ys, xs_pred2, ys_pred2)
+                for yy, xx in line_pts:
+                    if binary_image[yy, xx] == 0:
+                        flag = True
+                        break
+                if flag:
+                    s = "G0"
+                else:
+                    s = "G1"
+                if x != xs_pred1:
+                    s += f"X{x}"
+                    xs_pred1 = x
+                if y != ys_pred1:
+                    s += f"Y{y}"
+                    ys_pred1 = y
+                if s in ["G0", "G1"]:
+                    s = ""
+                if not flag:
+                    image = cv2.line(image,
+                                     (xs, ys), (xs_pred2, ys_pred2),
+                                     (0, 0, 0), thickness=1)
+                ys_pred2, xs_pred2 = ys, xs
+                s += "\n" + s.replace("G1", "G0") + "\n"
+                result += s
+    cv2.imwrite("output.nc.png", image)
     return result
 
 
-def get_gcode(material="plastic"):
-    # Plastic = S1000.00F1000.00
-    # Wood = = S950.00F4000.00
-    with open(f"begin_{material}.nc", 'r', encoding="UTF-8") as f:
+def get_gcode(speed: int = 4000, power: int = 95, loop: int = 1):
+    with open(f"begin.nc", 'r', encoding="UTF-8") as f:
         preamble = f.read()
-    with open(f"end_{material}.nc", 'r', encoding="UTF-8") as f:
+    with open(f"end.nc", 'r', encoding="UTF-8") as f:
         postamble = f.read()
-
-    if material == "wood":
-        speed = "S950.00F4000.00"
-    elif material == "plastic":
-        speed = "S1000.00F1000.00"
-    else:
-        raise "Error! material!"
-
-    optimized_points = optimize(filename="input.png", speed=speed)
+    speed = f"S{power * 10}.00F{speed}.00"
+    optimized_points = optimize(filename="input.png", speed=speed, loop=loop)
 
     with open("output.nc", 'w', encoding="UTF-8") as f:
         f.write(preamble)
@@ -104,5 +153,5 @@ def get_gcode(material="plastic"):
 
 
 if __name__ == "__main__":
-    # get_gcode() # plastic
-    get_gcode(material="wood")
+    get_gcode(**task)
+    winsound.Beep(1000, 1500)
