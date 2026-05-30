1ec365a68e0681d4219bb4208314d58db66d734a
xayam <xayam@yandex>
2026-05-30 07:12:21 +0300

    v1.12.21

--- docs/xml2html.py
+++ docs/xml2html.py
@@ -13,20 +13,20 @@ tasks = [
     },
 ]
 
+replacers = [
+    {
+        "from": "x/xmap/author/playground-games/ru-book.xml",
+        "to": "x/xmap/author/playground-games/ru-book.html",
+    }
+]
+
 for i in range(len(tasks)):
     xml_doc = etree.parse(tasks[i]["xml"])
     xsl_doc = etree.parse(tasks[i]["xsl"])
     transform = etree.XSLT(xsl_doc)
     tasks[i]["output"] = str(transform(xml_doc))
-    if i == 1:
-        file_output = tasks[0]["html"]
-        str_replace = tasks[0]["output"]
-        from_replace = tasks[1]["xml"]
-        to_replace = tasks[1]["html"]
-        str_replace = str_replace.replace(from_replace, to_replace)
-        with open(file_output, "w", encoding="utf-8") as f:
-            f.write(str_replace)
-    else:
-        with open(tasks[i]["html"], "w", encoding="utf-8") as f:
-            f.write(tasks[i]["output"])
+    for r in replacers:
+        tasks[i]["output"] = tasks[i]["output"].replace(r["from"], r["to"])
+    with open(tasks[i]["html"], "w", encoding="utf-8") as f:
+        f.write(tasks[i]["output"])
     print(f"✅ HTML успешно создан: {tasks[i]['html']}")
