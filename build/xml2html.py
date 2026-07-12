f152132a71965b9b3e79489b2cee38fd3d6285d1
xayam <xayam@yandex>
2026-07-12 21:45:44 +0300

    v1.17.3

--- build/xml2html.py
+++ build/xml2html.py
@@ -1,91 +1,26 @@
 import re
 import base64
+import json
 
 from lxml import etree
 
-mime_types = {
-    'png': 'png',
-    'jpg': 'jpeg',
-    'webp': 'webp'
-}
+with open("xml2html.json", 'r', encoding='utf-8') as f:
+    config = json.load(f)
+tasks = config["tasks"]
+string_replacers = config["string_replacers"]
+file_replacers = config["file_replacers"]
+mime_types = config["mime_types"]
 
-tasks = [
-    {
-        "base": "../docs/",
-        "xml": "../docs/index.xml",
-        "xsl": "../docs/x/index.xsl",
-        "html": "../docs/index.html"
-    },
-    {
-        "base": "../docs/x/xmap/author/playground-games/",
-        "xml": "../docs/x/xmap/author/playground-games/ru-book.xml",
-        "xsl": "../docs/x/xmap/author/playground-games/ru-book.xsl",
-        "html": "../docs/x/xmap/author/playground-games/ru-book.html"
-    },
-    {
-        "base": "../docs/x/xmap/author/playground-games/",
-        "xml": "../docs/x/xmap/author/playground-games/ru-book.fb2.xml",
-        "xsl": "../docs/x/xmap/author/playground-games/ru-book.xsl",
-        "html": "../docs/x/xmap/author/playground-games/ru-book.fb2.html"
-    },
-    {
-        "base": "../docs/x/xmap/author/playground-games/",
-        "xml": "../docs/x/xmap/author/playground-games/ru-book.pdf.xml",
-        "xsl": "../docs/x/xmap/author/playground-games/ru-book.xsl",
-        "html": "../docs/x/xmap/author/playground-games/ru-book.pdf.html"
-    },
-]
-
-string_replacers = [
-
-    {
-        "from": '<?xml version="1.0"?>',
-        "to": "<!DOCTYPE html>",
-    },
-    {
-        "from": "x/xmap/author/playground-games/ru-book.xml",
-        "to": "x/xmap/author/playground-games/ru-book.html",
-    }
-]
-
-file_replacers = [
-    {
-        "template": "{{{INDEX_DEFAULT_CSS}}}",
-        "file": "../docs/x/xstyle/resources/default.css",
-    },
-    {
-        "template": "{{{INDEX_STYLE_CSS}}}",
-        "file": "../docs/x/xstyle/resources/style.css",
-    },
-    {
-        "template": "{{{INDEX_SLIDER_CSS}}}",
-        "file": "../docs/x/xstyle/resources/slider.css",
-    },
-
-    {
-        "template": "{{{XGAME_DEFAULT_CSS}}}",
-        "file": "../docs/x/xmap/author/playground-games/xbook.xgame/resources/default.css",
-    },
-    {
-        "template": "{{{XGAME_STYLE_CSS}}}",
-        "file": "../docs/x/xmap/author/playground-games/xbook.xgame/resources/style.css",
-    },
-    {
-        "template": "{{{XGAME_PAGED}}}",
-        "file": "../docs/x/xmap/author/playground-games/xbook.xgame/resources/paged.polyfill.js",
-    },
-]
-
-for i in range(len(tasks)):
-    xml_doc = etree.parse(tasks[i]["xml"])
-    xsl_doc = etree.parse(tasks[i]["xsl"])
+for task in tasks:
+    xml_doc = etree.parse(task["base"] + task["xml"])
+    xsl_doc = etree.parse(task["base"] + task["xsl"])
     transform = etree.XSLT(xsl_doc)
-    tasks[i]["output"] = str(transform(xml_doc))
-    images = re.findall(r"src=\"(.+?)\.(png|jpg|webp)\"", tasks[i]["output"])
+    task["output"] = str(transform(xml_doc))
+    images = re.findall(r"src=\"(.+?)\.(png|jpg|webp)\"", task["output"])
     images = [
         {
             "orig": j[0] + "." + j[1],
-            "path": tasks[i]["base"] + j[0] + "." + j[1],
+            "path": task["base"] + j[0] + "." + j[1],
             "ext": j[1]
         } for j in images
     ]
@@ -97,18 +32,15 @@ for i in range(len(tasks)):
             base64_bytes = base64.b64encode(binary_data)
             base64_string = base64_bytes.decode('utf-8')
             src = f"data:image/{mime_type};base64,{base64_string}"
-        tasks[i]["output"] = tasks[i]["output"].replace(
+        task["output"] = task["output"].replace(
             f"src=\"{image['orig']}\"", f"src=\"{src}\"")
-        # print(f"src=\"{image['orig']}\"")
-        # print(tasks[i]["output"])
-        # sys.exit()
 
     for r in string_replacers:
-        tasks[i]["output"] = tasks[i]["output"].replace(r["from"], r["to"])
+        task["output"] = task["output"].replace(r["from"], r["to"])
     for r in file_replacers:
         with open(r["file"], "r", encoding="utf-8") as f:
             r_file = f.read()
-        tasks[i]["output"] = tasks[i]["output"].replace(r["template"], r_file)
-    with open(tasks[i]["html"], "w", encoding="utf-8") as f:
-        f.write(tasks[i]["output"])
-    print(f"✅ HTML успешно создан: {tasks[i]['html']}")
+        task["output"] = task["output"].replace(r["template"], r_file)
+    with open(task["base"] + task["html"], "w", encoding="utf-8") as f:
+        f.write(task["output"])
+    print(f"✅ HTML успешно создан: {task['base'] + task['html']}")
