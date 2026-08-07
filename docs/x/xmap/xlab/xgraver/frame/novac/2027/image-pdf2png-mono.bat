57f7c7ead5099312fd86ceffde37831ea5d828d0
xayam <xayam@yandex>
2026-08-07 20:41:39 +0300

    v1.17.22

---
+++ docs/x/xmap/xlab/xgraver/frame/novac/2027/image-pdf2png-mono.bat
@@ -0,0 +1,5 @@
+@echo off
+
+D:/Programs/ImageMagick/magick -density 600 E:\Xayam\XWork\XWorker\Projects\xarchive\XArchive\subprojects\org\xayam.ru\xayam.ru\docs\x\xmap\xlab\xgraver\novac\2027-10w\novac.2027.image.a4.pdf -colorspace gray -depth 3 E:\Xayam\XWork\XWorker\Projects\xarchive\XArchive\subprojects\org\xayam.ru\xayam.ru\docs\x\xmap\xlab\xgraver\novac\2027-10w\novac.2027.image.a4.png
+
+pause
